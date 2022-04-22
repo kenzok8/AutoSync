6f14c7e2e2137388e227e30ffc4ef23176a406cd
kenzok8 <kenzok8@gmail.com>
2022-04-22 20:05:50 +0800

    int

--- small.sh
+++ small.sh
@@ -16,19 +16,18 @@ rm -rf $1
 }
 
 git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall && mv -n openwrt-passwall/chinadns-ng openwrt-passwall/dns2socks openwrt-passwall/hysteria openwrt-passwall/ipt2socks openwrt-passwall/pdnsd-alt openwrt-passwall/trojan-go openwrt-passwall/trojan-plus openwrt-passwall/ssocks ./ ; rm -rf openwrt-passwall
-
 svn export --force https://github.com/immortalwrt/packages/trunk/net/brook
-svn co https://github.com/openwrt/packages/trunk/net/shadowsocks-libev
-svn co https://github.com/fw876/helloworld/trunk/simple-obfs
-svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust
-svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev
-svn co https://github.com/fw876/helloworld/trunk/trojan
-svn co https://github.com/fw876/helloworld/trunk/v2ray-core
-svn co https://github.com/fw876/helloworld/trunk/v2ray-geodata
-svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin
-svn co https://github.com/fw876/helloworld/trunk/v2raya
-svn co https://github.com/fw876/helloworld/trunk/xray-core
-svn co https://github.com/fw876/helloworld/trunk/xray-plugin
+svn export --force https://github.com/openwrt/packages/trunk/net/shadowsocks-libev
+svn export --force https://github.com/fw876/helloworld/trunk/simple-obfs
+svn export --force https://github.com/fw876/helloworld/trunk/shadowsocks-rust
+svn export --force https://github.com/fw876/helloworld/trunk/shadowsocksr-libev
+svn export --force https://github.com/fw876/helloworld/trunk/trojan
+svn export --force https://github.com/fw876/helloworld/trunk/v2ray-core
+svn export --force https://github.com/fw876/helloworld/trunk/v2ray-geodata
+svn export --force https://github.com/fw876/helloworld/trunk/v2ray-plugin
+svn export --force https://github.com/fw876/helloworld/trunk/v2raya
+svn export --force https://github.com/fw876/helloworld/trunk/xray-core
+svn export --force https://github.com/fw876/helloworld/trunk/xray-plugin
 
 bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
 bash diy/convert_translation.sh -a >/dev/null 2>&1

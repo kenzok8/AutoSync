10b35c6c0cc071c1bbcd83da44864d18c94250f5
有種 <39034242+kenzok8@users.noreply.github.com>
2022-04-26 00:12:24 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -69,6 +69,15 @@ svn co https://github.com/immortalwrt/packages/trunk/utils/filebrowser
 rm -rf ./*/.git & rm -rf ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 
+
+for e in $(ls -d luci-*/po); do
+	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
+		ln -s zh-cn $e/zh_Hans 2>/dev/null
+	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
+		ln -s zh_Hans $e/zh-cn 2>/dev/null
+	fi
+done
+
 exit 0
 
 

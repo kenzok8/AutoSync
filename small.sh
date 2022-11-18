3a3dce23086beee6f9d7b9825f9689ef8b9ecf1c
有種 <39034242+kenzok8@users.noreply.github.com>
2022-11-18 12:44:54 +0800

    Update small.sh

--- small.sh
+++ small.sh
@@ -15,7 +15,7 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall && mv -n openwrt-passwall/brook openwrt-passwall/chinadns-ng openwrt-passwall/dns2socks openwrt-passwall/dns2tcp openwrt-passwall/hysteria openwrt-passwall/ipt2socks openwrt-passwall/pdnsd-alt openwrt-passwall/trojan-go openwrt-passwall/trojan-plus openwrt-passwall/ssocks ./ ; rm -rf openwrt-passwall
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall && mv -n openwrt-passwall/chinadns-ng openwrt-passwall/dns2socks openwrt-passwall/dns2tcp openwrt-passwall/hysteria openwrt-passwall/ipt2socks openwrt-passwall/pdnsd-alt openwrt-passwall/trojan-go openwrt-passwall/trojan-plus openwrt-passwall/ssocks ./ ; rm -rf openwrt-passwall
 svn export --force https://github.com/openwrt/packages/trunk/net/shadowsocks-libev
 svn export --force https://github.com/fw876/helloworld/trunk/simple-obfs
 svn export --force https://github.com/fw876/helloworld/trunk/shadowsocks-rust
@@ -27,6 +27,7 @@ svn export --force https://github.com/fw876/helloworld/trunk/v2ray-plugin
 svn export --force https://github.com/fw876/helloworld/trunk/v2raya
 svn export --force https://github.com/fw876/helloworld/trunk/xray-core
 svn export --force https://github.com/fw876/helloworld/trunk/xray-plugin
+svn export https://github.com/kenzok8/jell/trunk/brook
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \

d9795e6d74a24e6da9671f55e5d1369dd6abc108
kenzok8 <kenzok8@gmail.com>
2023-02-05 13:46:03 +0000

    update

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -2,8 +2,8 @@
 # --------------------------------------------------------
 # Script for creating ACL file for each LuCI APP
 
-bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
-bash diy/convert_translation.sh -a >dev/null 2>&1
+rm -rf ./*/.git & rm -rf ./*/.gitattributes
+rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 
 rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
 rm -rf create_acl_for_luci.warn

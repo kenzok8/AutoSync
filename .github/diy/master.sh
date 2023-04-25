95a4f90b04004cd21025ab77e5f9b23caa93b282
有種 <39034242+kenzok8@users.noreply.github.com>
2023-04-25 22:06:39 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -53,7 +53,7 @@ git clone --depth 1 https://github.com/vernesong/OpenClash && mv -n OpenClash/lu
 git clone --depth 1 https://github.com/kenzok8/litte && mv -n litte/luci-theme-atmaterial_new litte/luci-theme-tomato ./ ; rm -rf litte
 git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
 git clone --depth 1 https://github.com/fw876/helloworld && mv -n helloworld/luci-app-ssr-plus ./ ; rm -rf helloworld
-git clone --depth 1 https://github.com/kenzok8/wall && mv -n wall/UnblockNeteaseMusic wall/gost wall/smartdns wall/adguardhome wall/filebrowser wall/lua-neturl wall/sagernet-core wall/tcping wall/redsocks2 wall/microsocks wall/naiveproxy ./ ; rm -rf wall
+git clone --depth 1 https://github.com/kenzok8/wall && mv -n wall/UnblockNeteaseMusic wall/gost wall/smartdns wall/adguardhome wall/filebrowser wall/lua-neturl wall/tcping wall/redsocks2 wall/microsocks wall/naiveproxy ./ ; rm -rf wall
 git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/luci-app-gost luci/applications/luci-app-filebrowser ./ ; rm -rf luci
 svn export https://github.com/kenzok8/jell/trunk/gn
 svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-ipkg

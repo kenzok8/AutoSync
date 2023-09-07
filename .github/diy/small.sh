cdbc30695e146b74f85e7a2299650fc85eadc40e
有種 <39034242+kenzok8@users.noreply.github.com>
2023-09-07 17:33:36 +0800

    Update small.sh

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -20,7 +20,6 @@ git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwa
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && mv -n passwall2/luci-app-passwall2 ./;rm -rf passwall2
 git clone --depth 1 https://github.com/fw876/helloworld && mv -n helloworld/{luci-app-ssr-plus,tuic-client} ./ ; rm -rf helloworld
 #svn export https://github.com/immortalwrt/packages/trunk/devel/gn
-svn export https://github.com/kenzok8/wall/trunk/gn
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-bypass
 
 sed -i \

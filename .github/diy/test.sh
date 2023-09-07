8786bf9bbf7ec1a5d59e253838231ec3588fff73
kenzok8 <kenzok8@gmail.com>
2023-09-07 16:00:32 +0000

    update

--- .github/diy/test.sh
+++ .github/diy/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 示例：使用环境变量来引用 matrix.packages 变量
-PLUGIN_VERSION=$("$matrix_packages" version | grep "$PKG_VERSION")
+PLUGIN_VERSION=$($matrix_packages version | grep "$PKG_VERSION")
 
 # 将版本号输出到标准输出
 echo "$PLUGIN_VERSION"

4856e9d174ea8f9a72ee470ed1cf269f7f8e5b21
kenzok8 <kenzok8@gmail.com>
2023-09-07 16:41:46 +0000

    update

--- .github/diy/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 使用 find 命令查找所有 Makefile 文件，并提取 PKG_VERSION 或 PKG_BASE_VERSION 变量的值
-find . -type f -name Makefile -exec grep -E 'PKG_(BASE_)?VERSION\s*?:=' {} \; | awk -F '[:=]' '{print $2}'
-

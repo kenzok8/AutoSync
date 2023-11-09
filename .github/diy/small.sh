274315263a3d45a304c26dc4d7c8ba24b95953b9
有種 <39034242+kenzok8@users.noreply.github.com>
2023-11-09 22:26:40 +0800

    Update small.sh

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -18,7 +18,7 @@ git clone --depth 1 https://github.com/kenzok8/wall && mv -n wall/* ./ ; rm -rf 
 git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
 git clone --depth 1 -b main https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && mv -n passwall2/luci-app-passwall2 ./;rm -rf passwall2
-git clone https://github.com/XiaoliChan/helloworld -b mosdns-improv && rm -rf helloworld/v2raya && mvdir helloworld
+git clone https://github.com/XiaoliChan/helloworld -b main && rm -rf helloworld/v2raya && mvdir helloworld
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages && mv -n passwall-packages/{sing-box,shadowsocks-rust} ./; rm -rf passwall-packages
 #svn export https://github.com/immortalwrt/packages/trunk/devel/gn
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-bypass

bbef922405b9e9c4001eb5333d9670c4cecdea7e
有種 <39034242+kenzok8@users.noreply.github.com>
2025-01-16 15:10:20 +0800

    Update small.sh

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -23,12 +23,12 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && 
 git clone --depth 1 https://github.com/fw876/helloworld && mv -n helloworld/{luci-app-ssr-plus,tuic-client,shadow-tls} ./ ; rm -rf helloworld
 git clone --depth 1 https://github.com/kiddin9/kwrt-packages && mv -n kwrt-packages/luci-app-bypass ./ ; rm -rf kwrt-packages
 git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo OpenWrt-mihomo && mv -n OpenWrt-mihomo/*mihomo ./ ; rm -rf OpenWrt-mihomo
-git clone --depth 1 https://github.com/muink/luci-app-homeproxy
+#git clone --depth 1 https://github.com/muink/luci-app-homeproxy
 git clone --depth 1 https://github.com/muink/openwrt-fchomo openwrt-fchomo && mv -n openwrt-fchomo/*homo ./ ; rm -rf openwrt-fchomo
 
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages && mv -n passwall-packages/{sing-box,shadowsocks-rust} ./; rm -rf passwall-packages
 #git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
-#git clone --depth 1 https://github.com/immortalwrt/homeproxy luci-app-homeproxy
+git clone --depth 1 https://github.com/immortalwrt/homeproxy luci-app-homeproxy
 #svn export https://github.com/immortalwrt/packages/trunk/devel/gn
 
 sed -i \

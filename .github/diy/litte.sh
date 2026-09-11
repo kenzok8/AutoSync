7dbdb0a724938350bc27a4b2a4be21b77495644b
有種 <39034242+kenzok8@users.noreply.github.com>
2026-09-11 22:20:14 +0800

    Modify luci-app-mosdns clone command
    Updated the cloning command for luci-app-mosdns to include additional files.

--- .github/diy/litte.sh
+++ .github/diy/litte.sh
@@ -72,7 +72,7 @@ git clone --depth 1 https://github.com/kenzok78/luci-theme-infinityfreedom ifit 
 git clone --depth 1 https://github.com/kenzok78/luci-design-bundle design-bundle && mv -n design-bundle/*design ./; rm -rf design-bundle
 git clone --depth 1 https://github.com/kenzok78/luci-app-amlogic amlogic && mv -n amlogic/luci-app-amlogic ./;rm -rf amlogic
 git clone --depth 1 https://github.com/kenzok8/openwrt-clashoo openwrt-clashoo && mv -n openwrt-clashoo/*clashoo ./; rm -rf openwrt-clashoo
-git clone --depth 1 -b v5 https://github.com/sbwml/luci-app-mosdns openwrt-mos && mv -n openwrt-mos/*mosdns ./ && rm -rf openwrt-mos
+git clone --depth 1 -b v5 https://github.com/sbwml/luci-app-mosdns openwrt-mos && mv -n openwrt-mos/{luci-app-mosdns,mosdns,geo2txt} ./ && rm -rf openwrt-mos
 git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
 git clone --depth 1 https://github.com/sbwml/luci-app-quickfile
 git clone --depth 1 -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
